53c8ed8ea5885a3b5fa2b4f1b0ea071e0714005a
andretan <andretan@stud.ntnu.no>
2023-09-08 23:34:03 +0200

    #3 added button that displays new joke when tapped

--- src/components/JokeBox.tsx
+++ src/components/JokeBox.tsx
@@ -1,3 +1,4 @@
+import { useState,useEffect } from "react";
 import {
   useQuery,
   QueryClient,
@@ -8,29 +9,66 @@ import {
 const queryClient = new QueryClient()
 
 function JokeBox() {
+
   return (
     // Provide the client to your App
     <QueryClientProvider client={queryClient}>
-      <Joke />
+      <Joke/>
     </QueryClientProvider>
   )
 }
 
 function Joke() {
-  const { isLoading, isError, error, data } = useQuery({
-    queryKey: ["repoData"],
-    queryFn: () =>
-      fetch("https://v2.jokeapi.dev/joke/Any").then((res) => res.json()),
-  })
 
-  if (isLoading) return "Loading..."
+  const [setUp, setSetUp] = useState("hei")
+  const [delivery, setDelivery] = useState("hei2")
+  const [isLoading2, setIsLoading] = useState(false)
+  const [isError2, setIsError] = useState(false)
+  const [flip, setFlip] = useState(false)
 
-  if (isError) return "An error has occurred: " + { error }
+  const handleClick = () => {
+    setFlip(!flip)
+  }
+
+  if (!flip) {
+    const { isLoading, isError, data } = useQuery({
+      queryKey: ["jokes1"],
+      queryFn: () =>
+        fetch("https://v2.jokeapi.dev/joke/Any").then((res) => res.json()),
+    })
+    useEffect(() => {
+      if (data) {
+        setSetUp(data.setup);
+        setDelivery(data.delivery);
+      }
+      setIsLoading(isLoading);
+      setIsError(isError);
+    }, [data, isLoading, isError]);
+  } else if (flip) {
+    const { isLoading, isError, data } = useQuery({
+      queryKey: ["jokes2"],
+      queryFn: () =>
+        fetch("https://v2.jokeapi.dev/joke/Any").then((res) => res.json()),
+    })
+    useEffect(() => {
+      if (data) {
+        setSetUp(data.setup);
+        setDelivery(data.delivery);
+      }
+      setIsLoading(isLoading);
+      setIsError(isError);
+    }, [data, isLoading, isError]);
+  }
+
+  if (isLoading2) return "Loading..."
+
+  if (isError2) return "Something went wrong"
 
   return (
     <div>
-      <p>{data.setup}</p>
-      <p>{data.delivery}</p>
+      <p>{setUp}</p>
+      <p>{delivery}</p>
+      <button onClick={handleClick}>Get new joke</button>
     </div>
   )
 }
