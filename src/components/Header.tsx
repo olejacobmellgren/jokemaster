3c3be356d36304c74e6e0302d0c3eb601a80439b
andretan <andretan@stud.ntnu.no>
2023-09-11 20:11:58 +0200

    fixed categories in Header-component
    Co-authored-by: olejacobmellgren <olejme@stud.ntnu.no>

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import '../assets/Header.css';
-import { set } from 'firebase/database';
 
 type CheckboxProps = {
   name: string;
@@ -24,23 +23,23 @@ function Checkbox({ name, checked, onChange }: CheckboxProps) {
 }
 
 function Header() {
-  const [Dropdown, setDropdown] = React.useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
-  const [Programming, setProgramming] = React.useState(false);
-  const [Misc, setMisc] = React.useState(false);
-  const [Dark, setDark] = React.useState(false);
-  const [Pun, setPun] = React.useState(false);
-  const [Spooky, setSpooky] = React.useState(false);
-  const [Christmas, setChristmas] = React.useState(false);
+  const [programming, setProgramming] = useState(false);
+  const [pun, setPun] = useState(false);
+  const [spooky, setSpooky] = useState(false);
+  const [christmas, setChristmas] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("Category", getCategory())
+  }, [dropdown])
 
   function handleDropdown() {
-    setDropdown(!Dropdown);
+    setDropdown(!dropdown);
   }
   
   function handleCheck({checked, setChecked}: any) {
     setProgramming(false);
-    setMisc(false);
-    setDark(false);
     setPun(false);
     setSpooky(false);
     setChristmas(false);
@@ -49,17 +48,13 @@ function Header() {
   }
 
   function getCategory() {
-    if (Programming) {
+    if (programming) {
       return "Programming";
-    } else if (Misc) {
-      return "Misc";
-    } else if (Dark) {
-      return "Dark";
-    } else if (Pun) {
+    } else if (pun) {
       return "Pun";
-    } else if (Spooky) {
+    } else if (spooky) {
       return "Spooky";
-    } else if (Christmas) {
+    } else if (christmas) {
       return "Christmas";
     } else {
       return "Category";
@@ -72,14 +67,12 @@ function Header() {
         <div>
           <button className="DropdownButton" onClick={handleDropdown}>{getCategory()}</button>
         </div>
-        {Dropdown ? 
+        {dropdown ? 
           <div className="Dropdown">
-            <Checkbox name="Programming" checked={Programming} onChange={() => handleCheck({ checked: Programming, setChecked: setProgramming })}/>
-            <Checkbox name="Misc" checked={Misc} onChange={() => handleCheck({ checked: Misc, setChecked: setMisc })}/>
-            <Checkbox name="Dark" checked={Dark} onChange={() => handleCheck({ checked: Dark, setChecked: setDark })}/>
-            <Checkbox name="Pun" checked={Pun} onChange={() => handleCheck({ checked: Pun, setChecked: setPun })}/>
-            <Checkbox name="Spooky" checked={Spooky} onChange={() => handleCheck({ checked: Spooky, setChecked: setSpooky })}/>
-            <Checkbox name="Christmas" checked={Christmas} onChange={() => handleCheck({ checked: Christmas, setChecked: setChristmas })}/>
+            <Checkbox name="Programming" checked={programming} onChange={() => handleCheck({ checked: programming, setChecked: setProgramming })}/>
+            <Checkbox name="Pun" checked={pun} onChange={() => handleCheck({ checked: pun, setChecked: setPun })}/>
+            <Checkbox name="Spooky" checked={spooky} onChange={() => handleCheck({ checked: spooky, setChecked: setSpooky })}/>
+            <Checkbox name="Christmas" checked={christmas} onChange={() => handleCheck({ checked: christmas, setChecked: setChristmas })}/>
           </div> : null}
         <p>
           JOKEMASTER-3000
