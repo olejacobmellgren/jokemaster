7e77e2192aefe53f27c30342545ea13a2799c08c
andretan <andretan@stud.ntnu.no>
2023-09-19 14:13:11 +0200

    #19 ran prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Header from "./components/Header.tsx";
 import { Route, Routes } from "react-router-dom";
 import Mainpage from "./pages/MainPage";
-import { useQuery, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import JokeBox from "./components/JokeBox.tsx";
 import { CategoryProvider } from "./context/CategoryContext.tsx";
 import DarkModeProvider from "./context/DarkModeContext.tsx";
@@ -20,43 +24,47 @@ interface Joke {
 const queryClient = new QueryClient();
 
 function JokeComponent() {
-
   const categories = ["Programming", "Pun", "Spooky", "Christmas"];
   const jokesCached_2 = localStorage.getItem("Programming");
-  
+
   if (!jokesCached_2) {
     useQuery({
       queryKey: ["apiData", "Categories"],
       queryFn: async () => {
         let randomJokesString = localStorage.getItem("randomJokes");
-        let allJokesList: Joke[] = randomJokesString ? JSON.parse(randomJokesString) : [];
-  
-        await Promise.all(categories.map(async (category) => {
-          const res = await fetch(`https://v2.jokeapi.dev/joke/${category}?amount=10`);
-          const data = await res.json();
-          const jokesList = data.jokes; // length == 10
-    
-          const jokesCached = localStorage.getItem(category);
-    
-          if (!jokesCached) {
-            localStorage.setItem(category, JSON.stringify(jokesList));
-            allJokesList = allJokesList.concat(jokesList);
-          }
-        }));
+        let allJokesList: Joke[] = randomJokesString
+          ? JSON.parse(randomJokesString)
+          : [];
+
+        await Promise.all(
+          categories.map(async (category) => {
+            const res = await fetch(
+              `https://v2.jokeapi.dev/joke/${category}?amount=10`,
+            );
+            const data = await res.json();
+            const jokesList = data.jokes; // length == 10
+
+            const jokesCached = localStorage.getItem(category);
+
+            if (!jokesCached) {
+              localStorage.setItem(category, JSON.stringify(jokesList));
+              allJokesList = allJokesList.concat(jokesList);
+            }
+          }),
+        );
         allJokesList.sort(() => Math.random() - 0.5);
-    
+
         localStorage.setItem("randomJokes", JSON.stringify(allJokesList));
-    
+
         return allJokesList;
-      }
-    })
+      },
+    });
   }
 
-  return null
+  return null;
 }
 
 function App() {
-
   return (
     <QueryClientProvider client={queryClient}>
       <DarkModeProvider>
