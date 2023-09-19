0df83a34d2e8bb0e97cc86cb7bdcb310f1aff886
andretan <andretan@stud.ntnu.no>
2023-09-19 17:00:22 +0200

    #14 mock tests are finished

--- src/__tests__/Header.test.tsx
+++ src/__tests__/Header.test.tsx
@@ -5,18 +5,21 @@ import { CategoryProvider } from '../context/CategoryContext';
 import userEvent from '@testing-library/user-event';
 
 
-test('Show dropdown when clicked', () => {
+test('Show dropdown when clicked', async () => {
     const {getByRole} = render(
       <CategoryProvider>
           <Header/>
       </CategoryProvider>
     );
+
     const dropDown = getByRole("button");
-    userEvent.click(dropDown);
+    await userEvent.click(dropDown);
     const programming = screen.getByText("Programming");
     expect(programming).toBeTruthy(); // The text is found
+
     const pun = screen.getByText("Pun");
     expect(pun).toBeTruthy(); // The text is found
+
     const favorites = screen.getByText("Favorites");
     expect(favorites).toBeTruthy(); // The text is found
 })
@@ -27,6 +30,7 @@ test('Title is found', () => {
           <Header/>
       </CategoryProvider>
     );
+    
     const title = getByText("JOKEMASTER-3000");
     expect(title).toBeTruthy(); // The text is found
 })
