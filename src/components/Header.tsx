4292c33ea018dc3678de61ce73c235ad8697ad8c
andretan <andretan@stud.ntnu.no>
2023-11-24 03:04:57 +0100

    #26 updated design and logic of dropdownmenu
    - Managed to get rid of a lot of code in Header.tsx by doing this
    - Removed the contextProvider we used, since currentFilter is passed as a prop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,179 +1,30 @@
-import { useEffect, useState } from "react";
 import "../assets/Header.css";
-import { useCategory } from "../context/CategoryContext"; // Importing useCategory hook
-import { Category } from "../context/CategoryContext"; // Importing Category type
 import { DarkModeProvider } from "../context/DarkModeContext"; // Importing DarkModeContext
 import { useContext } from "react";
-import Checkbox from "./Checkbox";
+import DropdownMenu from "./DropdownMenu";
 
-function Header() {
-  const { changeCategory } = useCategory(); // Getting changeCategory from the context
+type HeaderProps = {
+  onSelect: (option: string) => void;
+};
 
+
+function Header({onSelect}: HeaderProps) {
   const { darkMode, setDarkMode } = useContext(DarkModeProvider);
-  const [dropdown, setDropdown] = useState(false);
-  const [programming, setProgramming] = useState(false);
-  const [pun, setPun] = useState(false);
-  const [spooky, setSpooky] = useState(false);
-  const [christmas, setChristmas] = useState(false);
-  const [favorites, setFavorites] = useState(false);
-  const [category, setCategory] = useState("");
+  const categories = ["Programming", "Pun", "Spooky", "Christmas", "Favorites"];
 
-  // check if lastCategory exists in localStorage. If so, update category to that category. If not, set category to "Category"
-  useEffect(() => {
-    if (!programming && category == "") {
-      const lastCategory = localStorage.getItem("lastCategory");
-      if (
-        lastCategory !== null &&
-        lastCategory !== "DummyCategory" &&
-        lastCategory !== "Category"
-      ) {
-        changeCategory(lastCategory as Category);
-
-        let tempCheckbox: (value: boolean) => void;
-        if (lastCategory === "Programming") {
-          tempCheckbox = setProgramming;
-        } else if (lastCategory === "Pun") {
-          tempCheckbox = setPun;
-        } else if (lastCategory === "Spooky") {
-          tempCheckbox = setSpooky;
-        } else if (lastCategory === "Christmas") {
-          tempCheckbox = setChristmas;
-        } else {
-          tempCheckbox = setFavorites;
-        }
-        tempCheckbox(true);
-        setCategory(lastCategory as Category);
-        setDropdown(false);
-      } else {
-        changeCategory("Category");
-      }
-    }
-  }, []);
-
-  function handleDropdown() {
-    setDropdown(!dropdown);
-  }
-
-  // handles the checkbox logic, setting the checked checkbox to true and changing the category
-  function handleCheck({
-    checked,
-    categoryType,
-    setChecked,
-  }: {
-    checked: boolean;
-    categoryType: Category;
-    setChecked: (value: boolean) => void;
-  }) {
-    setProgramming(false);
-    setPun(false);
-    setSpooky(false);
-    setChristmas(false);
-    setFavorites(false);
-    setChecked(!checked);
-    setCategory(categoryType);
-
-    setDropdown(false);
-
-    const wasProgramming = programming == true && categoryType == "Programming";
-    const wasPun = pun == true && categoryType == "Pun";
-    const wasSpooky = spooky == true && categoryType == "Spooky";
-    const wasChristmas = christmas == true && categoryType == "Christmas";
-    const wasFavorites = favorites == true && categoryType == "Favorites";
-
-    // checks if the user unchecked a category - if so, set category to 'Category'.
-    if (wasProgramming || wasPun || wasSpooky || wasChristmas || wasFavorites) {
-      changeCategory("Category");
-    } else {
-      changeCategory(categoryType);
-    }
-  }
-
-  function getCategory() {
-    if (programming) {
-      return "Programming";
-    } else if (pun) {
-      return "Pun";
-    } else if (spooky) {
-      return "Spooky";
-    } else if (christmas) {
-      return "Christmas";
-    } else if (favorites) {
-      return "Favorites";
-    } else {
-      return "Category";
-    }
+  const handleFilterChange = (value: string) => {
+    onSelect(value)
   }
 
   return (
     <>
       <div className="header">
-        <div className="dropdownButtonWrapper">
-          <button className="dropdownButton" onClick={handleDropdown}>
-            <label className="DdBlabel">{getCategory()}</label>
-            <i className="dropdownArrow"></i>
-          </button>
-          {dropdown ? (
-            <div className="dropdown">
-              <Checkbox
-                name="Programming"
-                checked={programming}
-                onChange={() =>
-                  handleCheck({
-                    checked: programming,
-                    categoryType: "Programming",
-                    setChecked: setProgramming,
-                  })
-                }
-              />
-              <Checkbox
-                name="Pun"
-                checked={pun}
-                onChange={() =>
-                  handleCheck({
-                    checked: pun,
-                    categoryType: "Pun",
-                    setChecked: setPun,
-                  })
-                }
-              />
-              <Checkbox
-                name="Spooky"
-                checked={spooky}
-                onChange={() =>
-                  handleCheck({
-                    checked: spooky,
-                    categoryType: "Spooky",
-                    setChecked: setSpooky,
-                  })
-                }
-              />
-              <Checkbox
-                name="Christmas"
-                checked={christmas}
-                onChange={() =>
-                  handleCheck({
-                    checked: christmas,
-                    categoryType: "Christmas",
-                    setChecked: setChristmas,
-                  })
-                }
-              />
-              <Checkbox
-                name="Favorites"
-                checked={favorites}
-                onChange={() =>
-                  handleCheck({
-                    checked: favorites,
-                    categoryType: "Favorites",
-                    setChecked: setFavorites,
-                  })
-                }
-              />
-            </div>
-          ) : null}
+        <div className="dropdownMenu">
+          <DropdownMenu filter="Category" options={categories} onSelect={(value) => handleFilterChange(value)} />
         </div>
         <div className="logo">
-          <p>JOKEMASTER-3000</p>
+          <p>JOKEMASTER-</p>
+          <p>3000</p>
         </div>
         <div className="toggleWrapper">
           <input
--- src/components/JokeBox.tsx
+++ src/components/JokeBox.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect, ChangeEvent } from "react";
 import "../assets/JokeBox.css";
-import { useCategory } from "../context/CategoryContext";
 import favorite from "../assets/images/favorite.png";
 import noFavorite from "../assets/images/no-favorite.png";
 
@@ -14,8 +13,12 @@ interface Joke {
   // Add other properties if present in your JSON objects
 }
 
-function JokeBox() {
-  const { selectedCategory } = useCategory();
+type JokeboxProps = {
+  currentFilter: string;
+};
+
+function JokeBox({currentFilter} : JokeboxProps) {
+  
 
   const [counter, setCounter] = useState(0); // Counter for jokes from different categories. Goes up to 9
   const [favoriteCounter, setFavoriteCounter] = useState(0); // Counter for Favorites. Goes up to amount of favorites
@@ -63,35 +66,32 @@ function JokeBox() {
 
   // runs when the category is changed. Resets counter to 0 and displays first joke for that category
   useEffect(() => {
-    if (selectedCategory !== "DummyCategory") {
-      // an important check, without this we cant save the filter
-      localStorage.setItem("lastCategory", selectedCategory);
-      if (selectedCategory == "Category") {
-        // checks if current Category is "Category". If so, reset counter to 0 and display joke from randomJokes-state
-        const startCounter = sessionStorage.getItem("counterForRandomJokes");
-        if (startCounter === null) {
-          setCounterForRandomJokes(0);
-        } else {
-          setCounterForRandomJokes(parseInt(startCounter));
-        }
-
-        checkIfFavorite();
-        setJokeState(randomJokes);
+    if (currentFilter == "Category") {
+      // checks if current Category is "Category". If so, reset counter to 0 and display joke from randomJokes-state
+      const startCounter = sessionStorage.getItem("counterForRandomJokes");
+      if (startCounter === null) {
+        setCounterForRandomJokes(0);
       } else {
-        // else, reset counter to 0 and display joke from jokes-state
-        setCounter(0);
-        setFavoriteCounter(0);
-
-        //Add all jokes in category to jokelist
-        let jokesFromCategory: Joke[] = [];
-        const jokesCached = localStorage.getItem(`${selectedCategory}`);
-        if (jokesCached) {
-          jokesFromCategory = JSON.parse(jokesCached) as Joke[];
-        }
-        setJokesFromCategory(jokesFromCategory);
+        setCounterForRandomJokes(parseInt(startCounter));
       }
+
+      checkIfFavorite();
+      setJokeState(randomJokes);
+    } else {
+      // else, reset counter to 0 and display joke from jokes-state
+      setCounter(0);
+      setFavoriteCounter(0);
+
+      //Add all jokes in category to jokelist
+      let jokesFromCategory: Joke[] = [];
+      const jokesCached = localStorage.getItem(`${currentFilter}`);
+      if (jokesCached) {
+        jokesFromCategory = JSON.parse(jokesCached) as Joke[];
+      }
+      setJokesFromCategory(jokesFromCategory);
     }
-  }, [selectedCategory]);
+    
+  }, [currentFilter]);
 
   useEffect(() => {
     checkIfFavorite();
@@ -100,14 +100,14 @@ function JokeBox() {
 
   // runs when the counter is updated. Ensures that the joke to be displayed is rendered instantly to website
   useEffect(() => {
-    if (selectedCategory == "Category") {
+    if (currentFilter == "Category") {
       // checks if current Category is "Category". If so, display joke from randomJokes-state
       setJokeState(randomJokes);
       sessionStorage.setItem(
         "counterForRandomJokes",
         String(counterForRandomJokes),
       );
-    } else if (selectedCategory == "Favorites") {
+    } else if (currentFilter == "Favorites") {
       // checks if current Category is "Favorites". If so, display joke from favorites-state
       setJokeState(favorites);
     } else {
@@ -134,12 +134,12 @@ function JokeBox() {
 
   // checks if right limit is reached - if not, increase counter by 1
   function handleRightClick() {
-    if (selectedCategory === "Category") {
+    if (currentFilter === "Category") {
       if (counterForRandomJokes === 39) {
         return;
       }
       setCounterForRandomJokes((prevCounter) => prevCounter + 1);
-    } else if (selectedCategory === "Favorites") {
+    } else if (currentFilter === "Favorites") {
       if (
         favoriteCounter === favorites.length - 1 ||
         favorites.length - 1 === -1
@@ -157,12 +157,12 @@ function JokeBox() {
 
   // checks if left limit is reached - if not, decrease counter by 1
   function handleLeftClick() {
-    if (selectedCategory == "Category") {
+    if (currentFilter == "Category") {
       if (counterForRandomJokes === 0) {
         return;
       }
       setCounterForRandomJokes((prevCounter) => prevCounter - 1);
-    } else if (selectedCategory === "Favorites") {
+    } else if (currentFilter === "Favorites") {
       if (favoriteCounter === 0) {
         return;
       }
@@ -178,9 +178,9 @@ function JokeBox() {
   // returns the start index(dependent on category)  + current counter-value.
   function jokeIndex() {
     let index = counter;
-    if (selectedCategory == "Category") {
+    if (currentFilter == "Category") {
       index = 0 + counterForRandomJokes; // use counter for random jokes instead
-    } else if (selectedCategory === "Favorites") {
+    } else if (currentFilter === "Favorites") {
       index = 0 + favoriteCounter;
     }
     return index;
@@ -190,9 +190,9 @@ function JokeBox() {
     const index = jokeIndex();
     let joke: Joke;
 
-    if (selectedCategory == "Category") {
+    if (currentFilter == "Category") {
       joke = randomJokes[index];
-    } else if (selectedCategory == "Favorites") {
+    } else if (currentFilter == "Favorites") {
       joke = favorites[index];
     } else {
       joke = jokesFromCategory[index];
@@ -231,7 +231,7 @@ function JokeBox() {
       }
       favorites.splice(indexToRemove, 1);
       setFavorites(favorites);
-      if (selectedCategory === "Favorites") {
+      if (currentFilter === "Favorites") {
         if (favoriteCounter !== favorites.length) {
           setJokesFromCategory(favorites);
           setJokeState(favorites);
@@ -257,7 +257,7 @@ function JokeBox() {
     ) as HTMLSelectElement;
     const [id, index] = JSON.parse(event.target.value); // Deserialize the string into an array
     setCounter(index);
-    if (selectedCategory === "Favorites") {
+    if (currentFilter === "Favorites") {
       setFavoriteCounter(index);
     }
     const selectedJoke = jokesFromCategory.find(
@@ -273,15 +273,15 @@ function JokeBox() {
   return (
     <>
       <div className="jokeboxWrapper">
-        <div className="jokeboxTop">
-          {selectedCategory === "Category" ? null : (
+        {currentFilter === "Category" ? null : (
+          <div className="jokeboxTop">
             <select
-              id="selectJoke"
-              value="default"
-              onChange={(event) => {
-                setCounter(parseInt(event.target.value));
-                handleSelectJoke(event);
-              }}
+            id="selectJoke"
+            value="default"
+            onChange={(event) => {
+              setCounter(parseInt(event.target.value));
+              handleSelectJoke(event);
+            }}
             >
               <option value="default" disabled>
                 Select specific joke
@@ -295,9 +295,8 @@ function JokeBox() {
                 </option>
               ))}
             </select>
-          )}
-        </div>
-
+          </div>
+        )}
         <div className="jokebox">
           <div>
             <button
@@ -309,13 +308,13 @@ function JokeBox() {
           </div>
 
           <div className="joke">
-            {selectedCategory === "Favorites" && favorites.length === 0 ? (
+            {currentFilter === "Favorites" && favorites.length === 0 ? (
               <p>You have no favorites</p>
             ) : setUp !== "" ? (
               <p>{setUp}</p>
             ) : null}
 
-            {selectedCategory === "Favorites" && favorites.length === 0 ? (
+            {currentFilter === "Favorites" && favorites.length === 0 ? (
               <p></p>
             ) : (
               <p>{delivery}</p>
@@ -344,7 +343,7 @@ function JokeBox() {
           </div>
 
           <div>
-            {selectedCategory === "Favorites" &&
+            {currentFilter === "Favorites" &&
             favorites.length === 0 ? null : isFavorite ? (
               <img
                 onClick={handleFavorite}
@@ -371,11 +370,11 @@ function JokeBox() {
           </div>
         </div>
         <div className="counter">
-          {selectedCategory === "Category" ? (
+          {currentFilter === "Category" ? (
             <p>
               {counterForRandomJokes + 1} / {randomJokes.length}
             </p>
-          ) : selectedCategory === "Favorites" ? (
+          ) : currentFilter === "Favorites" ? (
             <div>
               {favoriteCounter + 1 === 1 && favorites.length === 0 ? (
                 <p></p>
